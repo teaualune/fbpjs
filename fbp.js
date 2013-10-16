731c9f957a35345df3b4633b52f00011346fa448
teaualune <teaualune@gmail.com>
2013-10-16 21:19:25 +0800

    modify some terms

--- fbp.js
+++ fbp.js
@@ -11,15 +11,17 @@
                 } else {
                     // setter i.e. constructor
                     // if 1st argument is an array, we construct several instances
-                    var names;
+                    var name;
                     if ('string' === typeof arguments[0]) {
-                        names = [ arguments[0] ];
+                        name = arguments[0];
                     } else {
-                        names = arguments[0];
+                        name = arguments[0].name;
                     }
-                    for (var i = 0; i < names.length; i = i + 1) {
-                        _c[names[i]] = {
-                            name: names[i],
+                    if (_c[name]) {
+                        throw 'component has already defined';
+                    } else {
+                        _c[name] = {
+                            name: name,
                             addInput: function (input, order) {
                                 var that = this;
                                 this.inputs[order] = input;
@@ -43,64 +45,70 @@
             },
             setup: function (name, constructor) {
                 // graph constructor
-                var F = {
-                        name: name,
-                        components: [],
-                        inPorts: [],
-                        inN: 0,
-                        outPorts: [],
-                        outN: 0,
-                        outputs: [],
-                        callback: null,
-                        arcs: {},
-                        start: function (name, order) {
+                var graphName = name,
+                    components = [],
+                    inPorts = [],
+                    inN = 0,
+                    outPorts = [],
+                    outN = 0,
+                    arcs = {},
+                    F = {
+                        init: function (name, order) {
                             var component = FBP.component(name);
-                            component.graph = this.name;
-                            this.components.push(name);
-                            this.inPorts.push({
+                            component.graph = graphName;
+                            components.push(name);
+                            inPorts.push({
                                 component: name,
                                 order: order
                             });
-                            this.inN = this.inN + 1;
+                            inN = inN + 1;
                         },
-                        arc: function (inPort, inOrder, outPort, outOrder) {
+                        connect: function (inPort, inOrder, outPort, outOrder) {
                             var outComponent = FBP.component(outPort);
-                            if (!outComponent.graph || outComponent.graph !== this.name) {
-                                outComponent.graph = this.name;
-                                this.components.push(name);
+                            if (!outComponent.graph || outComponent.graph !== graphName) {
+                                outComponent.graph = graphName;
+                                components.push(outPort);
                             }
-                            this.arcs[inPort + inOrder] = {
+                            arcs[inPort + inOrder] = {
                                 component: outPort,
                                 order: outOrder
                             };
                         },
                         end: function (name, order) {
-                            this.outPorts.push({
+                            outPorts.push({
                                 component: name,
                                 order: order
                             });
-                            this.outN = this.outN + 1;
-                        },
-                        invoke: function () {
-                            var i = 0;
-                            this.callback = arguments[arguments.length - 1];
-                            for (i; i < arguments.length - 1; i = i + 1) {
-                                FBP.component(this.inPorts[i].component).addInput(arguments[i], this.inPorts[i].order);
-                            }
-                        },
-                        getNext: function (prev, prevOrder) {
-                            return this.arcs[prev + prevOrder] || 'end';
-                        },
-                        sendOutput: function (output, outOrder) {
-                            this.outputs[outOrder] = output;
-                            if (this.outputs.length === this.outN) {
-                                // end execution!
-                                this.callback.apply(FBP.graph(this.name), [ null ].concat(this.outputs));
-                                FBP.cleanup(this.name);
-                            }
+                            outN = outN + 1;
                         }
                     };
                 constructor(F);
+                F.name = graphName;
+                F.components = components;
+                F.inPorts = inPorts;
+                F.inN = inN;
+                F.outPorts = outPorts;
+                F.outN = outN;
+                F.outputs = [];
+                F.arcs = arcs;
+                F.go = function () {
+                    var i = 0;
+                    this.callback = arguments[arguments.length - 1];
+                    for (i; i < arguments.length - 1; i = i + 1) {
+                        FBP.component(this.inPorts[i].component).addInput(arguments[i], this.inPorts[i].order);
+                    }
+                };
+                F.getNext = function (prev, prevOrder) {
+                    return this.arcs[prev + prevOrder] || 'end';
+                };
+                F.sendOutput = function (output, outOrder) {
+                    this.outputs[outOrder] = output;
+                    if (this.outputs.length === this.outN) {
+                        // end execution!
+                        this.callback.apply(FBP.graph(this.name), [ null ].concat(this.outputs));
+                        FBP.cleanup(this.name);
+                    }
+                };
                 _g[arguments[0]] = F;
             },
             invokeComponent: function (component) {
